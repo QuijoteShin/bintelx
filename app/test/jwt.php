7c636900dd96f36466e245828f3abb8a95f056e3
QuijoteShin <quijote.shin@gmail.com>
2025-05-22 02:30:21 -0400

    JWT app

--- app/test/jwt.php
+++ app/test/jwt.php
@@ -1,28 +1,29 @@
 <?php require_once '../../bintelx/WarmUp.php';
-$secretKey = 's3z$!tk27';
+$secretKey = 'woz.min..';
 $mode = $argv[1];
 
 use \bX\Args;
 new Args();
-if(isset(Args::$OPT['create'])) {
-  $userId = $argv[2];
-  $payload = array(date("mdhs"), $userId);
-  $jwt = new \bX\JWT(null, $secretKey);
-  $jwt->setHeader(array('alg' => 'HS256', 'typ' => 'JWT'));
-  $jwt->setPayload($payload);
-  $token = $jwt->generateJWT();
-  echo "Token created: $token" . PHP_EOL;
-} else if (isset(Args::$OPT['process'])) {
-  $token = $argv[2];
-  $jwt = new \bX\JWT($token, $secretKey);
-  $isSignatureValid = $jwt->validateSignature();
-  if ($isSignatureValid) {
-    $payload = $jwt->getPayload();
-    $userId = $payload[1];
-    echo "User ID: $userId" . PHP_EOL;
-  } else {
-    echo "Invalid signature" . PHP_EOL;
-  }
+
+$userId = $argv[2];
+$userId = 1;
+$payload = [date("mdhs"), "id" => $userId];
+$jwt = new \bX\JWT($secretKey);
+$jwt->setHeader(array('alg' => 'HS256', 'typ' => 'JWT'));
+$jwt->setPayload($payload);
+$token = $jwt->generateJWT();
+echo "Token created: $token" . PHP_EOL;
+
+# validate
+# $token = $argv[2];
+$jwt = new \bX\JWT($secretKey, $token);
+$isSignatureValid = $jwt->validateSignature();
+if ($isSignatureValid) {
+  $payload = $jwt->getPayload();
+  $userId = $payload["id"];
+  $profile = new \bX\Profile();
+  $profile->load(["account_id" => $userId]);
+  echo "User ID:" . \bX\Profile::$account_id . PHP_EOL;
 } else {
-  echo "Invalid mode. Usage: php cli.php create|process" . PHP_EOL;
+  echo "Invalid signature" . PHP_EOL;
 }
