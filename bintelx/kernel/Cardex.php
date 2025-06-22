447ce1b9a148a077016542f001ad9e1640cc6cc9
QuijoteShin <quijote.shin@gmail.com>
2025-06-21 23:59:03 -0400

    ++ cardex regex and module control

--- bintelx/kernel/Cardex.php
+++ bintelx/kernel/Cardex.php
@@ -12,17 +12,28 @@ print_r($files);
 class Cardex {
   public static function search($path, $pattern, $flags = GLOB_NOSORT | GLOB_BRACE) {
     $path = realpath($path); # WARNING SECURITY RISK
-    $ptrn = $path . DIRECTORY_SEPARATOR . '*/'. $pattern;
+    $ptrn = $path . DIRECTORY_SEPARATOR . $pattern;
     $files = glob($ptrn, $flags);
+    if ($files === false) {
+      Log::logWarning("Cardex::search - glob() returned an error for pattern: " . $ptrn);
+      return [];
+    }
+
     $files = array_map(function ($file) use ($path) {
       $real = realpath($file);
       $real_base = realpath($path);
       $relative = str_replace($real_base, '', $real);
-      $module = explode('/', $relative); # "/module_id/" || "/module_id/mod2_id/api"
+      $relative = ltrim($relative, DIRECTORY_SEPARATOR);
+      $module_parts = explode('/', $relative); # "/module_id/" || "/module_id/mod2_id/api"
+      $module = '_default';
+      if (count($module_parts) > 1) {
+        $module = (strtolower($module_parts[0]) === 'custom' && isset($parts[1])) ? $module_parts[1] : $module_parts[0];
+      }
       return [
           'real' => $real,
           'relative' => $relative,
-          'module'   => $module
+          'module'   => $module,
+          'module_parts'   => $module_parts,
       ];
     }, $files);
 
